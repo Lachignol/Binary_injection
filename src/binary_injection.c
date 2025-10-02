2565c9d5e4cefd04c2d2f66290d5c8f2b379d944
Alexandre Scordilis <ascordil@f6r5s10.clusters.42paris.fr>
2025-10-02 19:43:44 +0200

    execute ok

--- src/binary_injection.c
+++ src/binary_injection.c
@@ -10,17 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "injection.h"
+#include "../include/injection.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 
-int	main(void)
-{
-	FILE *fptr;
+int main(int count, char **argv) {
 
-// Create a file
-	fptr = fopen("filename.txt", "w");
+  (void)count;
+  char fileName[] = "/tmp/fileXXXXXX";
+  size_t writeBytes;
+  int fd = mkstemp(fileName);
+  if (fd == -1) {
+    perror("Erreur lors de la creation du fichier temporaire");
+    return 1;
+  }
 
-// Close the file
-	binary_file.write(test);
-	fclose(fptr);
+  chmod(fileName, 0700);
+  writeBytes = write(fd, binary_injection, binary_injection_len);
+  if (writeBytes != binary_injection_len) {
+    perror("write");
+    close(fd);
+    unlink(fileName);
+    return 1;
+  }
+  close(fd);
+  if (chmod(fileName, 0700) == -1) {
+    perror("chmod");
+    unlink(fileName);
+    return 1;
+  }
+  // faire un fork
+  execv(fileName, argv);
 
+  perror("execv");
+  unlink(fileName);
+  return 1;
 }
